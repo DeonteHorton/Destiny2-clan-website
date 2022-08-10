506f4d8fe7a162fb41d2e9d2e6bceea6cd36a991
Deonte Horton <52177300+DeonteHorton@users.noreply.github.com>
2022-08-10 01:34:54 -0500

    Update index.js

--- API/index.js
+++ API/index.js
@@ -8,7 +8,7 @@ const router = require('./app/routes/router');
 const PORT = 3006;
 
  app.listen(PORT,()=>{
-    console.log(`Server at http://localhost:${PORT}`);
+    console.log(`Server at http://104.248.234.208/:${PORT}`);
 })
 
 app.use(cors());
@@ -20,21 +20,21 @@ app.use(bodyParser.json());
 
 app.get('/',(req,res)=>{
     res.json({
-    "All people":"http://localhost:3006/api/accounts",
-    "Collect Members data":"http://localhost:3006/api/accounts/members_data",
-    "Search for account":"http://localhost:3006/api/accounts/search_account/username&password",
-    "Find by User Name":"http://localhost:3006/api/accounts/username/username",
-    "Find by First name": "http://localhost:3006/api/accounts/by_fname/fname",
-    "Find by Last name": "http://localhost:3006/api/accounts/by_lname/lname",
-    "Find by age":"http://localhost:3006/api/accounts/by_age/age",
-    "Find by email":"http://localhost:3006/api/accounts/email/email",
-    "Find by guardian":"http://localhost:3006/api/accounts/guardian/guardian",
-    "Find by classtype":"http://localhost:3006/api/accounts/class/class_id",
-    "Get all contacts":"http://localhost:3006/api/contacts",
-    "Find by Name":"http://localhost:3006/api/contacts/name/name",
-    "Find by email":"http://localhost:3006/api/contacts/email/email",
-    "Find user comment":"http://localhost:3006/api/contacts/user_comment/name",
-    "Find by id":"http://localhost:3006/api/contacts/by_id/id"
+    "All people":"http://104.248.234.208:3006/api/accounts",
+    "Collect Members data":"http://104.248.234.208:3006/api/accounts/members_data",
+    "Search for account":"http://104.248.234.208:3006/api/accounts/search_account/username&password",
+    "Find by User Name":"http://104.248.234.208:3006/api/accounts/username/username",
+    "Find by First name": "http://104.248.234.208:3006/api/accounts/by_fname/fname",
+    "Find by Last name": "http://104.248.234.208:3006/api/accounts/by_lname/lname",
+    "Find by age":"http://104.248.234.208:3006/api/accounts/by_age/age",
+    "Find by email":"http://104.248.234.208:3006/api/accounts/email/email",
+    "Find by guardian":"http://104.248.234.208:3006/api/accounts/guardian/guardian",
+    "Find by classtype":"http://104.248.234.208:3006/api/accounts/class/class_id",
+    "Get all contacts":"http://104.248.234.208:3006/api/contacts",
+    "Find by Name":"http://104.248.234.208:3006/api/contacts/name/name",
+    "Find by email":"http://104.248.234.208:3006/api/contacts/email/email",
+    "Find user comment":"http://104.248.234.208:3006/api/contacts/user_comment/name",
+    "Find by id":"http://104.248.234.208:3006/api/contacts/by_id/id"
     })
 });
 app.post('/post',(req,res)=>{
